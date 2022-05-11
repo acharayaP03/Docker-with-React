e6e08700f23647b0536781c000cb295408628c5c
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-05-11 17:49:45 +1000

    changed on feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Redeploy app with docker and aws using travis ci</p>
+        <p>
+          This was changed on feature branch.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
